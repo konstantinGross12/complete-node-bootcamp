1fd8c51c3ed929f23dba2d215059508474536ec1
Konstantin Gross <51492554+konstantinGross12@users.noreply.github.com>
2024-06-24 15:07:57 +0200

    commit

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const textIn = fs.readFileSync('./txt/input.txt', 'utf8');
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+
+fs.writeFileSync('./txt/output.txt', textOut);
+
+console.log(textOut);
+
+console.log('File written!');
